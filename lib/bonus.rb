e347aeb1ae3aea118f3c2430816051d336b157de
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-31 20:06:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-def bonus(status)
+def bonus
   epic_tragedy[:montague][:hero][:status]= "dead"
   epic_tragedy[:capulet][:heroine][:status]= "dead"
 end
