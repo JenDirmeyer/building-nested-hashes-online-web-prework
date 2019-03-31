3da029a6f6f1d7a35375fa3f49562c3af2f3ba9d
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-31 19:57:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:first_friend => {:name => "Bevolio", :age => "17", :attitude =>"worried"}
+        {:first_friend => {:name => "Bevolio", :age => "17", :attitude =>"worried"},
           :second_friend => {:name => "Mercutio", :age => "18", :attitude =>"hot-headed"}
         }
       ]
